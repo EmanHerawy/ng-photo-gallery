68d4390ebec93a9227b57a014e4f34237d2f676f
Eman Herawy <emisky_vip@yahoo.com>
2018-01-10 06:38:23 +0200

    fix deletion bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
-import { ColorPickerComponent } from './color-picker/color-picker';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DrawBoardComponent } from './draw-board/draw-board';
-import { PhotoGalleryComponent } from "./photo-gallery/photo-gallery";
+import { PhotoGalleryComponent } from './photo-gallery/photo-gallery';
 
 @NgModule({
   declarations: [
-    AppComponent, DrawBoardComponent, ColorPickerComponent, PhotoGalleryComponent
+    AppComponent,  PhotoGalleryComponent
   ],
   imports: [
     BrowserModule
